7b5955b7e6a95eac13d33d17a6af17e66eaeae6e
Deun Lee <54697735+deunlee@users.noreply.github.com>
2019-12-04 01:13:01 +0900

    Add barcode scanner

--- app/src/main/java/com/crenu/library_manager/BookActivity.java
+++ app/src/main/java/com/crenu/library_manager/BookActivity.java
@@ -1,14 +1,18 @@
 package com.crenu.library_manager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Toast;
 
 public class BookActivity extends AppCompatActivity {
 
@@ -23,10 +27,28 @@ public class BookActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                IntentIntegrator codeScanner = new IntentIntegrator(BookActivity.this);
+                codeScanner.setBeepEnabled(false);
+                codeScanner.setOrientationLocked(false);
+                codeScanner.setPrompt("책 바코드를 인식해 주세요.");
+                codeScanner.initiateScan();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "취소됨", Toast.LENGTH_LONG).show();
+            } else {
+                Snackbar.make(findViewById(R.id.BookManage), "책 스캔 완료 : " + result.getContents(), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
 }
